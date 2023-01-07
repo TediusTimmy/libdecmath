895c19311a9b4b0822fc400b8600276f91b405f8
Thomas DiModica <ricinwich@yahoo.com>
2023-01-06 22:55:48 -0700

    I discovered cppcheck, and decided to use it to apply MISRA standards to the code, starting with the muldiv file. I don't think that it has really improved the quality of the code, it has just been pedantic about trivialities. The code really isn't any more readable or less likely to be error-prone. But, it now passes through a linter. You could say the code has been made ... MISRAble.

--- dm_muldiv.c
+++ dm_muldiv.c
@@ -38,7 +38,7 @@ void dm_muldiv_loadFrom(dm_muldiv_t dest, uint64_t source)
 #else // DM_NO_128_BIT_TYPE
    dest[3] = 0;
    dest[2] = 0;
-   dest[1] = source >> 32;
+   dest[1] = source >> 32U;
    dest[0] = source;
 #endif
  }
@@ -64,23 +64,23 @@ void dm_muldiv_mulBy(dm_muldiv_t dest, uint64_t rhs)
       plus the low half of the current high product,
       plus the current low product.
 */
-   uint64_t low = rhs & 0xFFFFFFFF;
-   uint64_t high = rhs >> 32;
+   uint64_t low = rhs & 0xFFFFFFFFU;
+   uint64_t high = rhs >> 32U;
    uint64_t temp1 = low * dest[0];
    uint64_t temp2 = high * dest[0];
    dest[0] = temp1;
-   temp1 >>=32;
-   temp1 = temp1 + (temp2 & 0xFFFFFFFF) + low * dest[1];
-   temp2 >>= 32;
-   temp2 = temp2 + high * dest[1];
+   temp1 >>= 32U;
+   temp1 = temp1 + (temp2 & 0xFFFFFFFFU) + (low * dest[1]);
+   temp2 >>= 32U;
+   temp2 = temp2 + (high * dest[1]);
    dest[1] = temp1;
-   temp1 >>=32;
-   temp1 = temp1 + (temp2 & 0xFFFFFFFF) + low * dest[2];
-   temp2 >>= 32;
-   temp2 = temp2 + high * dest[2];
+   temp1 >>= 32U;
+   temp1 = temp1 + (temp2 & 0xFFFFFFFFU) + (low * dest[2]);
+   temp2 >>= 32U;
+   temp2 = temp2 + (high * dest[2]);
    dest[2] = temp1;
-   temp1 >>=32;
-   temp1 = temp1 + (temp2 & 0xFFFFFFFF) + low * dest[3];
+   temp1 >>= 32U;
+   temp1 = temp1 + (temp2 & 0xFFFFFFFFU) + (low * dest[3]);
    dest[3] = temp1;
 #endif
  }
@@ -96,78 +96,80 @@ void dm_muldiv_copy(dm_muldiv_t dest, dm_muldiv_t src)
 #ifdef DM_USE_SLOW_BINARY
 void dm_muldiv_shl1(dm_muldiv_t arg)
  {
-   uint64_t temp = ((uint64_t)arg[0]) << 1;
+   uint64_t temp = ((uint64_t)arg[0]) << 1U;
    arg[0] = temp;
-   temp >>= 32;
-   temp = temp | ((uint64_t)arg[1]) << 1;
+   temp >>= 32U;
+   temp = temp | ((uint64_t)arg[1]) << 1U;
    arg[1] = temp;
-   temp >>= 32;
-   temp = temp | ((uint64_t)arg[2]) << 1;
+   temp >>= 32U;
+   temp = temp | ((uint64_t)arg[2]) << 1U;
    arg[2] = temp;
-   temp >>= 32;
-   temp = temp | ((uint64_t)arg[3]) << 1;
+   temp >>= 32U;
+   temp = temp | ((uint64_t)arg[3]) << 1U;
    arg[3] = temp;
  }
 
 void dm_muldiv_shr1(dm_muldiv_t arg)
  {
    uint64_t temp = ((uint64_t)arg[3]);
-   arg[3] = temp >> 1;
-   temp <<= 32;
+   arg[3] = temp >> 1U;
+   temp <<= 32U;
    temp = temp | ((uint64_t)arg[2]);
-   arg[2] = temp >> 1;
-   temp <<= 32;
+   arg[2] = temp >> 1U;
+   temp <<= 32U;
    temp = temp | ((uint64_t)arg[1]);
-   arg[1] = temp >> 1;
-   temp <<= 32;
+   arg[1] = temp >> 1U;
+   temp <<= 32U;
    temp = temp | ((uint64_t)arg[0]);
-   arg[0] = temp >> 1;
+   arg[0] = temp >> 1U;
  }
 #else /* Use faster word division */
-void dm_muldiv_shl(dm_muldiv_t arg, int count)
+void dm_muldiv_shl(dm_muldiv_t arg, unsigned int count)
  {
-   if (32 < count)
+   unsigned int real_count = count;
+   if (32U < real_count)
     {
       arg[3] = arg[2];
       arg[2] = arg[1];
       arg[1] = arg[0];
       arg[0] = 0U;
-      count -= 32;
+      real_count -= 32U;
     }
-   uint64_t temp = ((uint64_t)arg[0]) << count;
+   uint64_t temp = ((uint64_t)arg[0]) << real_count;
    arg[0] = temp;
-   temp >>= 32;
-   temp = temp | ((uint64_t)arg[1]) << count;
+   temp >>= 32U;
+   temp = temp | ((uint64_t)arg[1]) << real_count;
    arg[1] = temp;
-   temp >>= 32;
-   temp = temp | ((uint64_t)arg[2]) << count;
+   temp >>= 32U;
+   temp = temp | ((uint64_t)arg[2]) << real_count;
    arg[2] = temp;
-   temp >>= 32;
-   temp = temp | ((uint64_t)arg[3]) << count;
+   temp >>= 32U;
+   temp = temp | ((uint64_t)arg[3]) << real_count;
    arg[3] = temp;
  }
 
-void dm_muldiv_shr(dm_muldiv_t arg, int count)
+void dm_muldiv_shr(dm_muldiv_t arg, unsigned int count)
  {
-   if (32 < count)
+   unsigned int real_count = count;
+   if (32U < real_count)
     {
       arg[0] = arg[1];
       arg[1] = arg[2];
       arg[2] = arg[3];
       arg[3] = 0U;
-      count -= 32;
+      real_count -= 32U;
     }
    uint64_t temp = ((uint64_t)arg[3]);
-   arg[3] = temp >> count;
-   temp <<= 32;
+   arg[3] = temp >> real_count;
+   temp <<= 32U;
    temp = temp | ((uint64_t)arg[2]);
-   arg[2] = temp >> count;
-   temp <<= 32;
+   arg[2] = temp >> real_count;
+   temp <<= 32U;
    temp = temp | ((uint64_t)arg[1]);
-   arg[1] = temp >> count;
-   temp <<= 32;
+   arg[1] = temp >> real_count;
+   temp <<= 32U;
    temp = temp | ((uint64_t)arg[0]);
-   arg[0] = temp >> count;
+   arg[0] = temp >> real_count;
  }
 #endif /* DM_USE_SLOW_BINARY */
 #endif /* DM_NO_128_BIT_TYPE */
@@ -179,27 +181,30 @@ void dm_muldiv_divBy(dm_muldiv_t lhs, uint64_t rhs, uint64_t* quo, uint64_t* rem
    *rem = *lhs % rhs;
 #else // DM_NO_128_BIT_TYPE
 #ifdef DM_USE_SLOW_BINARY
-   dm_muldiv_t d, m, n, q;
+   dm_muldiv_t d;
+   dm_muldiv_t m;
+   dm_muldiv_t n;
+   dm_muldiv_t q;
    dm_muldiv_loadFrom(d, rhs);
    dm_muldiv_loadFrom(m, 1U);
    dm_muldiv_copy(n, lhs);
    dm_muldiv_loadFrom(q, 0U);
-   while (dm_muldiv_less(d, n))
+   while (0 != dm_muldiv_less(d, n))
     {
       dm_muldiv_shl1(d);
       dm_muldiv_shl1(m);
     }
-   if (dm_muldiv_less(n, d))
+   if (0 != dm_muldiv_less(n, d))
     {
       dm_muldiv_shr1(d);
       dm_muldiv_shr1(m);
     }
 
-   while (!dm_muldiv_zero(m))
+   while (0 == dm_muldiv_zero(m))
     {
       dm_muldiv_sub(n, n, d);
       dm_muldiv_add(q, q, m);
-      while (dm_muldiv_less(n, d))
+      while (0 != dm_muldiv_less(n, d))
        {
          dm_muldiv_shr1(d);
          dm_muldiv_shr1(m);
@@ -208,81 +213,87 @@ void dm_muldiv_divBy(dm_muldiv_t lhs, uint64_t rhs, uint64_t* quo, uint64_t* rem
    dm_muldiv_extract(q, quo);
    dm_muldiv_extract(n, rem);
 #else /* Use faster word division */
-   if (0 == (lhs[3] | lhs[2])) // Is this a degenerate case?
+   if (0U == (lhs[3] | lhs[2])) // Is this a degenerate case?
     {
-      uint64_t n = (((uint64_t)lhs[1]) << 32) | lhs[0];
+      uint64_t n = (((uint64_t)lhs[1]) << 32U) | lhs[0];
       *quo = n / rhs;
       *rem = n % rhs;
-      return;
     }
-   dm_muldiv_t n, d, s, t;
-   dm_muldiv_copy(n, lhs);
-   dm_muldiv_loadFrom(d, rhs);
-   uint64_t dig1, dig2;
-
-   int count = 0;
-   uint64_t byt = 0xFF00000000000000ULL;
-   uint64_t bit = 0x8000000000000000ULL;
-   while (0 == (rhs & byt))
+   else
     {
-      byt >>= 8;
-      bit >>= 8;
-      count += 8;
-    }
-   while (0 == (rhs & bit))
-    {
-      bit >>= 1;
-      ++count;
-    }
+      dm_muldiv_t n;
+      dm_muldiv_t d;
+      dm_muldiv_t s;
+      dm_muldiv_t t;
+      dm_muldiv_copy(n, lhs);
+      dm_muldiv_loadFrom(d, rhs);
+      uint64_t dig1;
+      uint64_t dig2;
 
-   dm_muldiv_shl(n, count);
-   dm_muldiv_shl(d, count);
+      unsigned int count = 0U;
+      uint64_t byt = 0xFF00000000000000ULL;
+      uint64_t bit = 0x8000000000000000ULL;
+      while (0U == (rhs & byt))
+       {
+         byt >>= 8U;
+         bit >>= 8U;
+         count += 8U;
+       }
+      while (0U == (rhs & bit))
+       {
+         bit >>= 1U;
+         ++count;
+       }
 
-   s[3] = 0U;
-   s[2] = d[1];
-   s[1] = d[0];
-   s[0] = 0U;
+      dm_muldiv_shl(n, count);
+      dm_muldiv_shl(d, count);
 
-   dm_muldiv_copy(t, s);
+      s[3] = 0U;
+      s[2] = d[1];
+      s[1] = d[0];
+      s[0] = 0U;
 
-   dig1 = ((((uint64_t)n[3]) << 32) | n[2]) / d[1];
+      dm_muldiv_copy(t, s);
 
-   dm_muldiv_mulBy(t, dig1);
+      dig1 = ((((uint64_t)n[3]) << 32U) | n[2]) / d[1];
 
-   if (dm_muldiv_less(n, t)) // if t > n
-    {
-      --dig1;
-      dm_muldiv_sub(t, t, s);
-      if (dm_muldiv_less(n, t)) // According to Knuth, only need to do this twice.
+      dm_muldiv_mulBy(t, dig1);
+
+      if (0 != dm_muldiv_less(n, t)) // if t > n
        {
          --dig1;
          dm_muldiv_sub(t, t, s);
+         if (0 != dm_muldiv_less(n, t)) // According to Knuth, only need to do this twice.
+          {
+            --dig1;
+            dm_muldiv_sub(t, t, s);
+          }
        }
-    }
-   dm_muldiv_sub(n, n, t);
+      dm_muldiv_sub(n, n, t);
 
-   dm_muldiv_copy(s, d);
-   dm_muldiv_copy(t, d);
+      dm_muldiv_copy(s, d);
+      dm_muldiv_copy(t, d);
 
-   dig2 = ((((uint64_t)n[2]) << 32) | n[1]) / d[1];
+      dig2 = ((((uint64_t)n[2]) << 32U) | n[1]) / d[1];
 
-   dm_muldiv_mulBy(t, dig2);
+      dm_muldiv_mulBy(t, dig2);
 
-   if (dm_muldiv_less(n, t))
-    {
-      --dig2;
-      dm_muldiv_sub(t, t, s);
-      if (dm_muldiv_less(n, t))
+      if (0 != dm_muldiv_less(n, t))
        {
          --dig2;
          dm_muldiv_sub(t, t, s);
+         if (0 != dm_muldiv_less(n, t))
+          {
+            --dig2;
+            dm_muldiv_sub(t, t, s);
+          }
        }
-    }
-   dm_muldiv_sub(n, n, t);
+      dm_muldiv_sub(n, n, t);
 
-   *quo = (dig1 << 32) | dig2;
-   dm_muldiv_shr(n, count);
-   dm_muldiv_extract(n, rem);
+      *quo = (dig1 << 32U) | dig2;
+      dm_muldiv_shr(n, count);
+      dm_muldiv_extract(n, rem);
+    }
 #endif /* DM_USE_SLOW_BINARY */
 #endif /* DM_NO_128_BIT_TYPE */
  }
@@ -292,7 +303,7 @@ void dm_muldiv_extract(dm_muldiv_t source, uint64_t* dest)
 #ifndef DM_NO_128_BIT_TYPE
    *dest = *source;
 #else // DM_NO_128_BIT_TYPE
-   *dest = (((uint64_t)source[1]) << 32) | source[0];
+   *dest = (((uint64_t)source[1]) << 32U) | source[0];
 #endif
  }
 
@@ -304,13 +315,13 @@ void dm_muldiv_add(dm_muldiv_t dest, dm_muldiv_t lhs, dm_muldiv_t rhs) // dest m
 #else // DM_NO_128_BIT_TYPE
    uint64_t temp = ((uint64_t)lhs[0]) + rhs[0];
    dest[0] = temp;
-   temp >>= 32;
+   temp >>= 32U;
    temp = temp + lhs[1] + rhs[1];
    dest[1] = temp;
-   temp >>= 32;
+   temp >>= 32U;
    temp = temp + lhs[2] + rhs[2];
    dest[2] = temp;
-   temp >>= 32;
+   temp >>= 32U;
    temp = temp + lhs[3] + rhs[3];
    dest[3] = temp;
 #endif
@@ -323,14 +334,14 @@ void dm_muldiv_sub(dm_muldiv_t dest, dm_muldiv_t lhs, dm_muldiv_t rhs) // dest m
 #else // DM_NO_128_BIT_TYPE
    uint64_t temp = ((uint64_t)lhs[0]) - rhs[0];
    dest[0] = temp;
-   temp >>= 32;
-   temp = ((uint64_t)lhs[1]) - rhs[1] - (temp & 1);
+   temp >>= 32U;
+   temp = ((uint64_t)lhs[1]) - rhs[1] - (temp & 1U);
    dest[1] = temp;
-   temp >>= 32;
-   temp = ((uint64_t)lhs[2]) - rhs[2] - (temp & 1);
+   temp >>= 32U;
+   temp = ((uint64_t)lhs[2]) - rhs[2] - (temp & 1U);
    dest[2] = temp;
-   temp >>= 32;
-   temp = ((uint64_t)lhs[3]) - rhs[3] - (temp & 1);
+   temp >>= 32U;
+   temp = ((uint64_t)lhs[3]) - rhs[3] - (temp & 1U);
    dest[3] = temp;
 #endif
  }
@@ -338,9 +349,9 @@ void dm_muldiv_sub(dm_muldiv_t dest, dm_muldiv_t lhs, dm_muldiv_t rhs) // dest m
 int dm_muldiv_zero(dm_muldiv_t arg)
  {
 #ifndef DM_NO_128_BIT_TYPE
-   return 0 == *arg;
+   return 0U == *arg;
 #else // DM_NO_128_BIT_TYPE
-   return 0 == (arg[0] | arg[1] | arg[2] | arg[3]);
+   return 0U == (arg[0] | arg[1] | arg[2] | arg[3]);
 #endif
  }
 
@@ -349,8 +360,16 @@ int dm_muldiv_greater(dm_muldiv_t lhs, uint64_t rhs)
 #ifndef DM_NO_128_BIT_TYPE
    return *lhs > rhs;
 #else // DM_NO_128_BIT_TYPE
-   if (0 != (lhs[2] | lhs[3])) return 1;
-   return ((((uint64_t)lhs[1]) << 32) | lhs[0]) > rhs;
+   int result;
+   if (0U != (lhs[2] | lhs[3]))
+    {
+      result = 1;
+    }
+   else
+    {
+      result = ((((uint64_t)lhs[1]) << 32U) | lhs[0]) > rhs;
+    }
+   return result;
 #endif
  }
 
@@ -359,9 +378,23 @@ int dm_muldiv_less(dm_muldiv_t lhs, dm_muldiv_t rhs)
 #ifndef DM_NO_128_BIT_TYPE
    return *lhs < *rhs;
 #else // DM_NO_128_BIT_TYPE
-   if (lhs[3] != rhs[3]) return lhs[3] < rhs[3];
-   if (lhs[2] != rhs[2]) return lhs[2] < rhs[2];
-   if (lhs[1] != rhs[1]) return lhs[1] < rhs[1];
-   return lhs[0] < rhs[0];
+   int result;
+   if (lhs[3] != rhs[3])
+    {
+      result = lhs[3] < rhs[3];
+    }
+   else if (lhs[2] != rhs[2])
+    {
+      result = lhs[2] < rhs[2];
+    }
+   else if (lhs[1] != rhs[1])
+    {
+      result = lhs[1] < rhs[1];
+    }
+   else
+    {
+      result = lhs[0] < rhs[0];
+    }
+   return result;
 #endif
  }
